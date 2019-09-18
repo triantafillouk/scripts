531651109913add3dffb260103a65ecae40ca795
triantafillouk <52749996+triantafillouk@users.noreply.github.com>
2019-09-18 17:52:48 +0300

    new file initialize_second_disk in windows

---
+++ initialize_second_disk.ps1
@@ -0,0 +1,12 @@
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number                                                                                     
+                                                                                                                                                     $letters = 70..89 | ForEach-Object { [char]$_ }                                                                                                      
+$count = 0                                                                                                                                           
+$labels = "data-01","data-02"                                                                                                                        
+                                                                                                                                                     foreach ($disk in $disks) {                                                                                                                          
+    $driveLetter = $letters[$count].ToString()                                                                                                       
+    $disk |                                                                                                                                          
+    Initialize-Disk -PartitionStyle MBR -PassThru |                                                                                                  
+    New-Partition -UseMaximumSize -DriveLetter $driveLetter |                                                                                        
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force                                                        
+$count++                                                                                                                                             
+}
